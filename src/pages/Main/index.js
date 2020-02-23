993796521bebb433b0e6eb93320412e86ad08aab
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-02-22 23:09:17 -0300

    route navigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
@@ -39,13 +40,15 @@ export default class Main extends Component {
 
     const response = await api.get(`/repos/${newRepo}`);
 
-    this.setState({ loading: false });
-
     const data = {
       name: response.data.full_name,
     };
 
-    this.setState({ repositories: [...repositories, data], newRepo: '' });
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
   };
 
   render() {
@@ -76,7 +79,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="#">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import { Container } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default function Repository({ match }) {
+  const { repository } = match.params;
+  return <h1>Repository: {decodeURIComponent(repository)}</h1>;
 }
+
+Repository.defaultProps = {
+  match: { params: {} },
+};
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string.isRequired,
+    }),
+  }),
+};
