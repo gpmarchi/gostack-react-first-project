5913bc0a162a9c144162c5cf00d04fd375833c96
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-02-23 20:20:40 -0300

    repository details page

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  align-items: center;
+  color: #fff;
+  display: flex;
+  font-size: 30px;
+  font-weight: bold;
+  height: 100vh;
+  justify-content: center;
+`;
+
+export const Owner = styled.header`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+
+  a {
+    color: #7159c1;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
+  img {
+    border-radius: 50%;
+    margin-top: 20px;
+    width: 120px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    color: #666;
+    font-size: 14px;
+    line-height: 1.4;
+    margin-top: 5px;
+    max-width: 400px;
+    text-align: center;
+  }
+`;
